a63d349a0a6d127672b1b2d9c909b856bbaa687b
Aks <aksfromup93@gmail.com>
2025-11-15 16:25:11 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+// middlewares
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, Credentials: true })); // server allows to include cookies on request
+
+app.use("/api/inngest", serve({client:inngest,functions}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "success from backend" });
@@ -24,8 +33,6 @@ if (ENV.NODE_ENV == "production") {
   });
 }
 
-
-
 const startServer = async () => {
   try {
     await connectDB();
@@ -36,6 +43,5 @@ const startServer = async () => {
     console.error("Error starting the server ", error);
   }
 };
-
 
 startServer();
