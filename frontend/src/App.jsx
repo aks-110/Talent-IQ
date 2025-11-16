f7d7796e8eda66083dcaf1cfc2bc92230b66627b
Aks <aksfromup93@gmail.com>
2025-11-17 02:07:33 +0530

    video last session

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-  UserButton,
-  useUser,
-} from "@clerk/clerk-react";
-import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
+import { useUser } from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
-import ProblemsPage from "./pages/ProblemsPage";
+import { Navigate, Route, Routes } from "react-router";
 import DashboardPage from "./pages/DashboardPage";
+import HomePage from "./pages/HomePage";
 import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
@@ -41,6 +35,10 @@ function App() {
         <Route
           path="/problem/:id"
           element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/session/:id"
+          element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />}
         />
       </Routes>
 
